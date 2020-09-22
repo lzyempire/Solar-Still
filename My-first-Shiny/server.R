0a5e41944576b637445b0f1a323d2ae6a77e5b17
lzyempire <lzyempire@gmail.com>
2020-09-22 11:33:59 +0800

    Modified my Shiny apps, it works

--- My-first-Shiny/server.R
+++ My-first-Shiny/server.R
@@ -1,14 +1,19 @@
 #
 library(shiny)
-setwd("D:/R/Solar Still")
-library(lubridate)
-Still_BottomInter <- read.csv(file = "Bottom Interfacial Solar Still Daily Water Production.csv", header = TRUE, stringsAsFactors = FALSE)
-Still_SidewallInter <- read.csv(file = "Sidewall Interfacial Solar Still Daily Water Production.csv", header = TRUE, stringsAsFactors = FALSE)
-Still_FoamBottomInter <- read.csv(file = "Foam Bottom Interfacial Solar Still Daily Water Production.csv", header = TRUE, stringsAsFactors = FALSE)
-Still_SidewallHeat <- read.csv(file = "Sidewall Bottom Heating Solar Still Daily Water Production.csv", header = TRUE, stringsAsFactors = FALSE)
+library(RCurl)
+Still_BottomInter <- read.csv(text = getURL("https://raw.githubusercontent.com/lzyempire/Solar-Still/master/Bottom%20Interfacial%20Solar%20Still%20Daily%20Water%20Production.csv"), header = TRUE, stringsAsFactors = FALSE)
+Still_SidewallInter <- read.csv(text = getURL("https://raw.githubusercontent.com/lzyempire/Solar-Still/master/Sidewall%20Interfacial%20Solar%20Still%20Daily%20Water%20Production.csv"), header = TRUE, stringsAsFactors = FALSE)
+Still_FoamBottomInter <- read.csv(text = getURL("https://raw.githubusercontent.com/lzyempire/Solar-Still/master/Foam%20Bottom%20Interfacial%20Solar%20Still%20Daily%20Water%20Production.csv"), header = TRUE, stringsAsFactors = FALSE)
+Still_BottomHeat <- read.csv(text = getURL("https://raw.githubusercontent.com/lzyempire/Solar-Still/master/Bottom%20Heating%20Solar%20Still%20Daily%20Water%20Production.csv"), header = TRUE, stringsAsFactors = FALSE)
+Still_SidewallHeat <- read.csv(text = getURL("https://raw.githubusercontent.com/lzyempire/Solar-Still/master/Sidewall%20Bottom%20Heating%20Solar%20Still%20Daily%20Water%20Production.csv"), header = TRUE, stringsAsFactors = FALSE)
 
-Solar_Efficiency <- merge(merge(merge(Still_BottomInter[, c("Global_Energy_Tot", "Global_Efficiency")], Still_FoamBottomInter[, c("Global_Energy_Tot", "Global_Efficiency")], by = "Global_Energy_Tot", all = TRUE), Still_SidewallHeat[, c("Global_Energy_Tot", "Global_Efficiency")], by = "Global_Energy_Tot", all = TRUE), Still_SidewallInter[, c("Global_Energy_Tot", "Global_Efficiency")], by = "Global_Energy_Tot", all = TRUE)
-names(Solar_Efficiency) <- c("Solar_Energy", "Bottom_Interfacial", "Foam_Bottom_Interfacial", "Sidewall_Bottom_Heating", "Sidewall_Interfacial")
+library(dplyr)
+Solar_Efficiency <- Still_BottomHeat[, c("Global_Energy_Tot", "Global_Efficiency")] %>% 
+    full_join(Still_SidewallHeat[, c("Global_Energy_Tot", "Global_Efficiency")], by = "Global_Energy_Tot") %>% 
+    full_join(Still_BottomInter[, c("Global_Energy_Tot", "Global_Efficiency")], by = "Global_Energy_Tot") %>% 
+    full_join(Still_FoamBottomInter[, c("Global_Energy_Tot", "Global_Efficiency")], by = "Global_Energy_Tot") %>% 
+    full_join(Still_SidewallInter[, c("Global_Energy_Tot", "Global_Efficiency")], by = "Global_Energy_Tot")
+names(Solar_Efficiency) <- c("Solar_Energy", "Bottom_Heating", "Sidewall_Bottom_Heating", "Bottom_Interfacial", "Foam_Bottom_Interfacial", "Sidewall_Interfacial")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -19,7 +24,8 @@ shinyServer(function(input, output) {
         ymin <- input$sliderY[1]
         ymax <- input$sliderY[2]
         library(ggplot2)
-        Solar_Eff <- melt(Solar_Efficiency[, c(TRUE, input$show_1, input$show_2, input$show_3, input$show_4)], id = "Solar_Energy")
+        library(reshape2)
+        Solar_Eff <- melt(Solar_Efficiency[, c(TRUE, input$show_1, input$show_2, input$show_3, input$show_4, input$show_5)], id = "Solar_Energy")
         p <- ggplot(Solar_Eff, aes(Solar_Energy, value*100, color = variable))
         p + geom_point() + labs(x = "Solar Energy/kWh", y = "Efficiency/%") + geom_smooth(method="loess",se=FALSE) + scale_x_continuous(limits = c(xmin,xmax)) + scale_y_continuous(limits = c(ymin,ymax))
 
--- My-first-Shiny/ui.R
+++ My-first-Shiny/ui.R
@@ -8,10 +8,11 @@ shinyUI(fluidPage(
                         0, 10, value = c(0, 8)),
             sliderInput("sliderY", "Pick Minimum and Maximum of Efficiency Values(%)",
                         0, 100, value = c(0, 60)),
-            checkboxInput("show_1", "Show/Hide Bottom Interfacial Solar Still", value = TRUE),
-            checkboxInput("show_2", "Show/Hide Foam Bottom Interfacial Solar Still", value = TRUE),
-            checkboxInput("show_3", "Show/Hide Sidewall Bottom Heating Solar Still", value = TRUE),
-            checkboxInput("show_4", "Show/Hide Sidewall Interfacial Solar Still", value = TRUE)
+            checkboxInput("show_1", "Show/Hide Bottom Heating Solar Still", value = TRUE),
+            checkboxInput("show_2", "Show/Hide Sidewall Bottom Heating Solar Still", value = TRUE),
+            checkboxInput("show_3", "Show/Hide Bottom Interfacial Solar Still", value = TRUE),
+            checkboxInput("show_4", "Show/Hide Foam Bottom Interfacial Solar Still", value = TRUE),
+            checkboxInput("show_5", "Show/Hide Sidewall Interfacial Solar Still", value = TRUE)
         ),
 
         # Show a plot of the generated distribution
