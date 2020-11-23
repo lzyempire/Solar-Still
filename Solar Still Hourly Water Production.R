c313aac9b2fea6d3c8d3244160d6485fd29bd2eb
lzyempire <lzyempire@gmail.com>
2020-11-23 17:27:18 +0800

    save

--- Solar Still Hourly Water Production.R
+++ Solar Still Hourly Water Production.R
@@ -2,36 +2,41 @@
 setwd("D:/R/Solar Still")
 library(lubridate)
 library(dplyr)
-data_record <- as.Date("2020-07-03")
+data_record <- as.Date("2020-06-05")
 energy_record <- 5
 power_record <- 0
 
-SolarEnv_Day <- read.csv(file = "CR1000_BSRN1000_Day201009.csv", skip = 1, stringsAsFactors = FALSE)
+SolarEnv_Day <- read.csv(file = "CR1000_BSRN1000_Day201116.csv", skip = 1, stringsAsFactors = FALSE)
 SolarEnv_Day <- SolarEnv_Day[c(-1, -2), -(24:39)] ##Delete two rows of unit, 24-39 colunm
 SolarEnv_Day$TIMESTAMP <- as.Date(ymd_hms(SolarEnv_Day$TIMESTAMP))
 SolarEnv_Day <- SolarEnv_Day[SolarEnv_Day$TIMESTAMP >= data_record, ]
 SolarEnv_Day$Date <- SolarEnv_Day$TIMESTAMP - ddays(1)
 SolarEnv_Day[, 2:23] <- lapply(SolarEnv_Day[, 2:23], as.numeric)
-Solar_climate <- read.csv(file = "CR1000_BSRN1000_Min201009.csv", skip = 1, stringsAsFactors = FALSE)
+Solar_climate <- read.csv(file = "CR1000_BSRN1000_Min201116.csv", skip = 1, stringsAsFactors = FALSE)
 Solar_climate <- Solar_climate[c(-1, -2), -c(2, 14:19)] ##Delete two rows of unit, useless columns
 Solar_climate$TIMESTAMP <- ymd_hms(Solar_climate$TIMESTAMP)
 Solar_climate <- Solar_climate[date(Solar_climate$TIMESTAMP) >= data_record, ]
 Solar_climate[, 2:length(Solar_climate)] <- lapply(Solar_climate[, 2:length(Solar_climate)], as.numeric)
 
 cleandata <- function(SolarWater){
-  hourlywater <- rbind(rbind(c("0:00", 0), na.omit(SolarWater[2:length(SolarWater[, 1]), 1:2]), c("23:59", SolarWater[1, 2])))
-  names(hourlywater) <- c("Time", "WaterProduction")
-  hourlywater <- cbind(Date = SolarWater[1, 1], hourlywater)
-  hourlywater$Percentage <- as.numeric(hourlywater$WaterProduction)*100/as.numeric(SolarWater[1, 2])
-  for(i in 2:(length(SolarWater)/2)){
-    hourdata <- rbind(rbind(c("0:00", 0), na.omit(SolarWater[2:length(SolarWater[, (2*i-1)]), (2*i-1):(2*i)]), c("23:59", SolarWater[1, (2*i)])))
+    hourlywater <- rbind(na.omit(SolarWater[2:length(SolarWater[, 1]), 1:2]), c("23:59", SolarWater[1, 2]))
+    names(hourlywater) <- c("Time", "WaterProduction")
+    hourlywater <- hourlywater[hourlywater$WaterProduction != 0, ]
+    hourlywater <- rbind(c("0:00", 0), hourlywater)
+    hourlywater$WaterProduction <- as.numeric(hourlywater$WaterProduction)
+    hourlywater$Date <- ymd(SolarWater[1, 1])
+    hourlywater$Percentage <- hourlywater$WaterProduction*100/hourlywater$WaterProduction[length(hourlywater$WaterProduction)]
+    for(i in 2:(length(SolarWater)/2)){
+    hourdata <- rbind(na.omit(SolarWater[2:length(SolarWater[, (2*i-1)]), (2*i-1):(2*i)]), c("23:59", SolarWater[1, (2*i)]))
     names(hourdata) <- c("Time", "WaterProduction")
-    hourdata <- cbind(Date = SolarWater[1, (2*i-1)], hourdata)
-    hourdata$Percentage <- as.numeric(hourdata$WaterProduction)*100/as.numeric(SolarWater[1, (2*i)])
+    hourdata <- hourdata[hourdata$WaterProduction != 0, ]
+    hourdata <- rbind(c("0:00", 0), hourdata)
+    hourdata$WaterProduction <- as.numeric(hourdata$WaterProduction)
+    hourdata$Date <- ymd(SolarWater[1, (2*i-1)])
+    hourdata$Percentage <- hourdata$WaterProduction*100/hourdata$WaterProduction[length(hourdata$WaterProduction)]
     hourlywater <- rbind(hourlywater, hourdata)
   }
   hourlywater <- hourlywater[hourlywater$Date >= data_record, ]
-  hourlywater$Date <- ymd(hourlywater$Date)
   hourlywater$Time <- hm(hourlywater$Time)
   hourlywater$WaterProduction <- as.numeric(hourlywater$WaterProduction)*4/1000
   hourlywater$WaterEnergy <- hourlywater$WaterProduction/1.5
@@ -44,6 +49,7 @@ cleandata <- function(SolarWater){
   hourlywater$DeltaWaterEnergy <- c(0, diff(hourlywater$WaterEnergy))
   ##Set delta water energy < 0 data = 0
   hourlywater$DeltaWaterEnergy[hourlywater$DeltaWaterEnergy < 0] <- 0
+  #hourlywater$TransPercentage <- hourlywater$DeltaWaterEnergy/hourlywater$WaterEnergy
   hourlywater$AvgEfficiency <- hourlywater$WaterEnergy/hourlywater$SolarEnergy*100
   hourlywater$TransSolarPower <- hourlywater$DeltaSolarEnergy/hourlywater$DeltaTime
   hourlywater$DirectEnergy <- as.numeric(Solar_climate$Direct_Energy_Day[Solar_climate$TIMESTAMP %in% (hourlywater$Date + hourlywater$Time)])
@@ -66,6 +72,9 @@ cleandata <- function(SolarWater){
   return(hourlywater)
 }
 
+SolarWater_FoamSidewallInter <- read.csv(file = "Foam Sidewall Interfacial Solar Still Water Production.csv", header = FALSE, stringsAsFactors = FALSE)
+hourlywater_FoamSidewallInter <- cleandata(SolarWater_FoamSidewallInter)
+
 SolarWater_FoamBottomInter <- read.csv(file = "Foam Bottom Interfacial Solar Still Water Production.csv", header = FALSE, stringsAsFactors = FALSE)
 hourlywater_FoamBottomInter <- cleandata(SolarWater_FoamBottomInter)
 
@@ -82,11 +91,18 @@ SolarWater_BottomHeat <- read.csv(file = "Bottom Heating Solar Still Water Produ
 hourlywater_BottomHeat <- cleandata(SolarWater_BottomHeat)
 
 meltcol <- function(colnames){
-  mergecolselect <- rbind(rbind(rbind(rbind(cbind(hourlywater_BottomInter[, colnames], Still = "Bottom_Interfacial"), cbind(hourlywater_SidewallInter[, colnames], Still = "Sidewall_Interfacial")), cbind(hourlywater_FoamBottomInter[, colnames], Still = "Foam_Bottom_Interfacial")), cbind(hourlywater_SidewallHeat[, colnames], Still = "Sidewall_Bottom_Heating")), cbind(hourlywater_BottomHeat[, colnames], Still = "Bottom_Heating"))
+  mergecolselect <- rbind(rbind(rbind(rbind(rbind(
+    cbind(hourlywater_BottomHeat[, colnames], Still = "Bottom_Heating"),
+    cbind(hourlywater_SidewallHeat[, colnames], Still = "Sidewall_Bottom_Heating")), 
+    cbind(hourlywater_BottomInter[, colnames], Still = "Bottom_Interfacial")), 
+    cbind(hourlywater_FoamBottomInter[, colnames], Still = "Foam_Bottom_Interfacial")), 
+    cbind(hourlywater_SidewallInter[, colnames], Still = "Sidewall_Interfacial")), 
+    cbind(hourlywater_FoamSidewallInter[, colnames], Still = "Foam_Sidewall_Interfacial")) 
   return(mergecolselect)
 }
 
 hourlywater_Percentage <- meltcol(c("Time", "Percentage"))
+# hourlywater_TransPercentage <- meltcol(c("Time", "TransPercentage"))
 hourlywater_Production <- meltcol(c("Time", "WaterProduction"))
 hourlywater_AvgEfficiency <- meltcol(c("Time", "AvgEfficiency"))
 hourlywater_TransEfficiency <- meltcol(c("Time", "TransEfficiency"))
@@ -94,7 +110,7 @@ hourlywater_PowerEfficiency <- meltcol(c("TransSolarPower", "TransEfficiency"))
 hourlywater_DirectEfficiency <- meltcol(c("TransDirectPower", "TransEfficiency"))
 hourlywater_DiffuseEfficiency <- meltcol(c("TransDiffusePower", "TransEfficiency"))
 hourlywater_FitEfficiency <- meltcol(c("TransSolarPower", "FitEfficiency"))
-  
+ 
 hourlysolar <- Solar_climate[, c("TIMESTAMP", "Global_Energy_Day", "Direct_Energy_Day", "Diffuse_Energy_Day")]
 hourlysolar$Date <- as.Date(date(hourlysolar$TIMESTAMP))
 hourlysolar$Time <- as.numeric(hourlysolar$TIMESTAMP - as.POSIXct(hourlysolar$Date))/3600
@@ -104,14 +120,25 @@ hourlysolar <- hourlysolar[hourlysolar$Date != as.Date("2020-09-22"), ]
 hourlysolar$GlobalPercentage <- hourlysolar$Global_Energy_Day/hourlysolar$Global_Energy_Tot
 hourlysolar$DirectPercentage <- hourlysolar$Direct_Energy_Day/hourlysolar$Direct_Energy_Tot
 hourlysolar$DiffusePercentage <- hourlysolar$Diffuse_Energy_Day/hourlysolar$Diffuse_Energy_Tot
-hourlywater_solar <- na.omit(hourlysolar[, c("Time", "GlobalPercentage")])
-hourlywater_solar <- with(hourlywater_solar, tapply(GlobalPercentage, as.factor(Time), mean))
-hourlywater_solar <- cbind(as.data.frame.table(hourlywater_solar), Still = "Average_Global_Radiation")
-names(hourlywater_solar) <- names(hourlywater_Percentage)
-hourlywater_solar$Percentage <- as.numeric(hourlywater_solar$Percentage) * 100
-hourlywater_solar$Time <- as.numeric(as.character(hourlywater_solar$Time))
+hourlysolar$DeltaPercentage <- c(0, diff(hourlysolar$GlobalPercentage))
+##Set delta solar energy < 0 data = 0
+hourlysolar$DeltaPercentage[hourlysolar$DeltaPercentage < 0] <- 0
+Percentage_solar <- na.omit(hourlysolar[, c("Time", "GlobalPercentage")])
+Percentage_solar <- with(Percentage_solar, tapply(GlobalPercentage, as.factor(Time), mean))
+Percentage_solar <- cbind(as.data.frame.table(Percentage_solar), Still = "Average_Solar_Radiation")
+names(Percentage_solar) <- names(hourlywater_Percentage)
+Percentage_solar$Percentage <- as.numeric(Percentage_solar$Percentage) * 100
+Percentage_solar$Time <- as.numeric(as.character(Percentage_solar$Time))
 hourlywater_Percentage$Time <- hourlywater_Percentage$Time/hm("1:00")
-hourlywater_Percentage <- rbind(hourlywater_Percentage, hourlywater_solar)
+hourlywater_Percentage <- rbind(hourlywater_Percentage, Percentage_solar)
+# DeltaPercentage_solar <- na.omit(hourlysolar[, c("Time", "GlobalPercentage")])
+# DeltaPercentage_solar <- with(DeltaPercentage_solar, tapply(GlobalPercentage, as.factor(Time), mean))
+# DeltaPercentage_solar <- cbind(as.data.frame.table(DeltaPercentage_solar), Still = "Average_Solar_Radiation")
+# names(DeltaPercentage_solar) <- names(hourlywater_TransPercentage)
+# DeltaPercentage_solar$TransPercentage <- as.numeric(DeltaPercentage_solar$TransPercentage)
+# DeltaPercentage_solar$Time <- as.numeric(as.character(DeltaPercentage_solar$Time))
+# hourlywater_TransPercentage$Time <- hourlywater_TransPercentage$Time/hm("1:00")
+# hourlywater_TransPercentage <- rbind(hourlywater_TransPercentage, DeltaPercentage_solar)
 
 library(ggplot2)
 ##g <- ggplot(hourlywater_BottomInter, aes(Time/hm("1:00"), Percentage, color = WaterProduction))
@@ -127,12 +154,14 @@ library(ggplot2)
 ##DiffusePercentageBinomial <- ggplot(hourlysolar[(hourlysolar$DiffusePercentage <= 1) & (hourlysolar$DiffusePercentage >= 0), ], aes(Time, DiffusePercentage))
 ##DiffusePercentageBinomial + geom_point(alpha = 0.2) + stat_smooth(method = "glm", method.args = list(family = binomial), se=FALSE) + labs(x = "DayTime", y = "Normalized Solar Radiation Percentage")## + scale_x_continuous(limits = c(6,24))
 
-StillName_Interfacial <- c("Bottom_Interfacial", "Foam_Bottom_Interfacial", "Sidewall_Interfacial")
+StillName_Interfacial <- c("Bottom_Interfacial", "Foam_Bottom_Interfacial", "Sidewall_Interfacial", "Foam_Sidewall_Interfacial")
 
 PercentageBinomial <- ggplot(hourlywater_Percentage, aes(Time, Percentage/100, color = Still))
 PercentageBinomial + geom_point(alpha = 0.1) + 
   stat_smooth(method = "glm", method.args = list(family = binomial), se=FALSE) + 
   labs(x = "DayTime", y = "Normalized Production Percentage")## + scale_x_continuous(limits = c(6,24))
+# TransPercentage <- ggplot(hourlywater_TransPercentage, aes(Time, TransPercentage, color = Still))
+# TransPercentage + geom_point(alpha = 0.1) + geom_smooth(se = FALSE)
 ##Percentageloess <- ggplot(hourlywater_Percentage, aes(Time/hm("1:00"), Percentage, color = Still))
 ##Percentageloess + geom_point(alpha = 0.2) + stat_smooth(se=FALSE) + labs(x = "DayTime", y = "Normalized Production Percentage") + scale_x_continuous(limits = c(6,20))
 ##h <- ggplot(hourlywater_Production, aes(Time/hm("1:00"), WaterProduction, color = Still))
@@ -142,9 +171,11 @@ PercentageBinomial + geom_point(alpha = 0.1) +
 ##TransEff <- ggplot(hourlywater_TransEfficiency[(hourlywater_TransEfficiency$Time/hm("1:00")<18)&(hourlywater_TransEfficiency$Time/hm("1:00")>6), ], aes(Time/hm("1:00"), TransEfficiency, color = Still))
 ##TransEff + geom_point(alpha = 0.5) + geom_smooth(se = FALSE) + labs(x = "Time", y = "Hourly Transient Efficiency/%") + scale_y_continuous(limits = c(0,100))## + scale_x_continuous(limits = c(7,20))
 TransEff <- ggplot(hourlywater_TransEfficiency, aes(Time/hm("1:00"), TransEfficiency, color = Still))
-TransEff + geom_point(alpha = 0.2) + geom_smooth(se = FALSE) + labs(x = "Time", y = "Hourly Transient Efficiency/%") + scale_y_continuous(limits = c(10,75)) + scale_x_continuous(limits = c(7,20))
+TransEff + geom_point(alpha = 0.2) + geom_smooth(se = FALSE) + labs(x = "Time", y = "Hourly Transient Efficiency/%") + scale_y_continuous(limits = c(10,70), breaks=seq(0,100,10)) + scale_x_continuous(limits = c(8,20), breaks=seq(0,24,2))
+
+StillName_Interfacial <- c("Bottom_Interfacial", "Foam_Bottom_Interfacial", "Sidewall_Interfacial", "Foam_Sidewall_Interfacial")
 PowerEff <- ggplot(hourlywater_PowerEfficiency[hourlywater_PowerEfficiency$Still %in% StillName_Interfacial, ], aes(TransSolarPower*1000, TransEfficiency, color = Still))
-PowerEff + geom_point(alpha = 0.2) + geom_smooth(se = FALSE) + labs(x = "Solar Power/W", y = "Hourly Transient Efficiency/%") + scale_y_continuous(limits = c(10,75)) + scale_x_continuous(limits = c(100,1000))
+PowerEff + geom_point(alpha = 0.2) + geom_smooth(se = FALSE) + labs(x = "Solar Power/W", y = "Hourly Transient Efficiency/%") + scale_y_continuous(limits = c(10,80), breaks=seq(0,100,10)) + scale_x_continuous(limits = c(100,1000), breaks=seq(0,1000,200))
 
 DirectEff <- ggplot(hourlywater_DirectEfficiency[hourlywater_DirectEfficiency$Still %in% StillName_Interfacial, ], aes(TransDirectPower*1000, TransEfficiency, color = Still))
 DirectEff + geom_point(alpha = 0.2) + geom_smooth(se = FALSE) + labs(x = "Solar Direct Power/W", y = "Hourly Transient Efficiency/%") + scale_y_continuous(limits = c(10,75)) + scale_x_continuous(limits = c(50,900))
@@ -153,3 +184,16 @@ DiffuseEff + geom_point(alpha = 0.2) + geom_smooth(se = FALSE) + labs(x = "Solar
 FitEff <- ggplot(hourlywater_FitEfficiency[hourlywater_FitEfficiency$Still %in% StillName_Interfacial, ], aes(TransSolarPower*1000, FitEfficiency, color = Still))
 FitEff + geom_point(alpha = 0.2) + geom_smooth(se = FALSE) + labs(x = "Solar Power/W", y = "Hourly Fit Transient Efficiency/%") + scale_y_continuous(limits = c(10,75)) + scale_x_continuous(limits = c(100,1000))
 
+
+library(plotly)
+PercentageBinomial <- ggplot(hourlywater_Percentage, aes(Time, Percentage/100, color = Still)) +
+  geom_point(alpha = 0.1) + stat_smooth(method = "glm", method.args = list(family = binomial), se=FALSE) + 
+  labs(x = "DayTime/0:00", y = "Normalized Percentage/100%") + scale_x_continuous(breaks=seq(0,24,2))
+ggplotly(PercentageBinomial)
+
+
+
+TransEff <- ggplot(hourlywater_TransEfficiency, aes(Time/hm("1:00"), TransEfficiency, color = Still)) + 
+  geom_point(alpha = 0.2) + geom_smooth(se = FALSE) + labs(x = "Time", y = "Hourly Transient Efficiency/%") + 
+  scale_y_continuous(limits = c(10,70), breaks=seq(0,100,10)) + scale_x_continuous(limits = c(8,20), breaks=seq(0,24,2))
+ggplotly(TransEff)
--- Water Production.R
+++ Water Production.R
@@ -8,7 +8,7 @@ Still_SidewallHeat <- read.csv(file = "Sidewall Bottom Heating Solar Still Daily
 Still_FoamSidewallInter <- read.csv(file = "Foam Sidewall Interfacial Solar Still Daily Water Production.csv", header = TRUE, stringsAsFactors = FALSE)
 StillNames <- c("Bottom_Heating", "Sidewall_Bottom_Heating", "Bottom_Interfacial", "Foam_Bottom_Interfacial", "Sidewall_Interfacial", "Foam_Sidewall_Interfacial")
 
-SolarEnv_Day <- read.csv(file = "CR1000_BSRN1000_Day201022.csv", skip = 1, stringsAsFactors = FALSE)
+SolarEnv_Day <- read.csv(file = "CR1000_BSRN1000_Day201116.csv", skip = 1, stringsAsFactors = FALSE)
 SolarEnv_Day <- SolarEnv_Day[c(-1, -2), ] ##Delete two rows of unit
 SolarEnv_Day$TIMESTAMP <- as.Date(ymd_hms(SolarEnv_Day$TIMESTAMP))
 SolarEnv_Day$TIMESTAMP <- SolarEnv_Day$TIMESTAMP - ddays(1)
@@ -31,7 +31,6 @@ WaterProduction_Efficiency <- Still_BottomHeat[, c("TIMESTAMP", "Global_Efficien
   full_join(Still_FoamSidewallInter[, c("TIMESTAMP", "Global_Efficiency")], by = "TIMESTAMP") %>% 
   left_join(SolarEnv_Day[, c("TIMESTAMP", "Global_Energy_Tot", "Direct_Energy_Tot", "Diffuse_Energy_Tot")], by = "TIMESTAMP")
 names(WaterProduction_Efficiency) <- c("Date", StillNames, "Solar_Energy", "Direct_Energy", "Diffuse_Energy")
-write.csv(WaterProduction_Efficiency, file = "Water Efficiency.csv")
 Direct_Efficiency <- WaterProduction_Efficiency[WaterProduction_Efficiency$Direct_Energy >= 1, ]
 Diffuse_Efficiency <- WaterProduction_Efficiency[WaterProduction_Efficiency$Direct_Energy < 1, ]
 fit_Efficiency <- Still_BottomHeat[, c("TIMESTAMP", "fit_Efficiency")] %>% 
@@ -49,6 +48,7 @@ Water_Energy$Date <- ymd(Water_Energy$Date)
 Water_Eff <- melt(WaterProduction_Efficiency, id = "Date")
 Water_Eff$Date <- ymd(Water_Eff$Date)
 Solar_Eff <- melt(WaterProduction_Efficiency[, c("Solar_Energy", StillNames)], id = "Solar_Energy")
+names(Solar_Eff) <- c("Solar_Energy", "Still_Types", "Energy_Efficiency")
 Direct_Eff <- melt(Direct_Efficiency[, c("Direct_Energy", StillNames)], id = "Direct_Energy")
 Diffuse_Eff <- melt(Diffuse_Efficiency[, c("Diffuse_Energy", StillNames)], id = "Diffuse_Energy")
 Total_eff <- melt(WaterProduction_Efficiency[, c("Solar_Energy", "Direct_Energy", "Diffuse_Energy", StillNames)], id = c("Solar_Energy", "Direct_Energy", "Diffuse_Energy"))
@@ -58,7 +58,7 @@ fit_Eff <- melt(fit_Efficiency[, c("Solar_Energy", StillNames)], id = "Solar_Ene
 library(ggplot2)
 StillEnergy <- ggplot(Water_Energy, aes(Date, value, fill = variable))
 StillEnergy + geom_bar(stat = 'identity', position='dodge') + labs(x = "Date", y = "Energy/kWh") 
-SolarEff <- ggplot(Solar_Eff, aes(Solar_Energy, value*100, color = variable))
+SolarEff <- ggplot(Solar_Eff, aes(Solar_Energy, Energy_Efficiency*100, color = Still_Types))
 SolarEff + geom_point() + labs(x = "Solar Energy/kWh", y = "Efficiency/%") + geom_smooth(method="loess",se=FALSE)
 fitEff <- ggplot(fit_Eff, aes(Solar_Energy, value*100, color = variable))
 fitEff + geom_point() + labs(x = "Solar Energy/kWh", y = "Efficiency/%") + geom_smooth(method="loess",se=FALSE)
@@ -114,11 +114,80 @@ StillEnergy_plotly <- plot_ly(WaterProduction_Energy, x = ~Date, y = ~Solar_Radi
     barmode = 'group')
 StillEnergy_plotly
 
-SolarEff_plotly <- plot_ly(Solar_Eff, x = ~Solar_Energy, y = ~value, color = ~variable, trendline="lowess")
+SolarEff <- ggplot(Solar_Eff, aes(Solar_Energy, Energy_Efficiency*100, color = Still_Types)) + geom_point() + labs(x = "Solar Energy/kWh", y = "Efficiency/%") + geom_smooth(method="loess",se=FALSE)
+SolarEff_plotly <- ggplotly(SolarEff) 
 SolarEff_plotly
 
-DirDiffEff_plotly <- plot_ly(Total_eff, x = ~Direct_Energy, y = ~Diffuse_Energy, z = ~Efficiency*100, color = ~Still_Type, type = 'scatter3d')
+
+
+
+# BHEff_loess <- loess(value ~ Solar_Energy, data = Solar_Eff[Solar_Eff$variable == "Bottom_Heating", ], span = 0.75)
+# SolarEff_plotly <- plot_ly(Solar_Eff, x = ~Solar_Energy, y = ~value, color = ~factor(variable)) %>%
+#   add_lines(x = Solar_Eff$Solar_Energy[Solar_Eff$variable == "Bottom_Heating", ], y = predict(BHEff_loess), line=line.fmt, name="Span = 0.75") %>%
+#   layout(yaxis = list(title = 'Efficiency/%'), xaxis = list(title = 'Energy/kWh'))
+# SolarEff_plotly
+
+DDirDiffEff_plotly <- plot_ly(Total_eff, x = ~Direct_Energy, y = ~Diffuse_Energy, z = ~Efficiency*100, color = ~Still_Type, type = 'scatter3d') %>%
+  layout(scene = list(xaxis = list(title = "Direct Energy/kWh"),
+                      yaxis = list(title = "Diffuse Energy/kWh"),
+                      zaxis = list(title = "Energy Efficiency/%")))
 DirDiffEff_plotly
+
+
+
+mlrcoef <- function(DirDiffStill){
+  fit <- lm(Global_Efficiency ~ Direct_Energy_Tot + Diffuse_Energy_Tot, data = na.omit(DirDiffStill))
+  output <- c(coef(fit)[1], coef(fit)[2], coef(fit)[3], summary(fit)$r.squared)
+  names(output) <- c("Intercept", "Direct_Coef", "Diffuse_Coef", "R_squred")
+  return(output)
+}
+DirDiffCoef <- rbind(mlrcoef(Still_BottomHeat), 
+                     mlrcoef(Still_SidewallHeat), 
+                     mlrcoef(Still_BottomInter), 
+                     mlrcoef(Still_FoamBottomInter), 
+                     mlrcoef(Still_SidewallInter), 
+                     mlrcoef(Still_FoamSidewallInter))
+rownames(DirDiffCoef) <- StillNames
+
+
+
+# Weatherfit <- function(DirDiffStill){
+#   DirDiffStill$DirDiffRatio <- DirDiffStill$Direct_Energy_Tot/DirDiffStill$Diffuse_Energy_Tot
+#   DirDiffStill$DirDiff <- cut(DirDiffStill$DirDiffRatio, breaks = c(min(na.omit(DirDiffStill$DirDiffRatio)), 0.1, 1, max(na.omit(DirDiffStill$DirDiffRatio))), labels = c("Overcast", "Cloudy", "Clear"))
+#   output <- ggplot(data = na.omit(DirDiffStill), aes(Global_Energy_Tot, Global_Efficiency*100, color = DirDiff)) + 
+#     geom_point() + geom_smooth(method = "lm") + 
+#     labs(x = "Solar Energy/kWh", y = "Efficiency/%")## + scale_y_continuous(breaks=seq(0,60,5))
+#   return(ggplotly(output))
+# }
+# Plotly_BottomHeat <- Weatherfit(Still_BottomHeat)
+# Plotly_SidewallHeat <- Weatherfit(Still_SidewallHeat)
+# Plotly_BottomInter <- Weatherfit(Still_BottomInter)
+# Plotly_FoamBottomInter <- Weatherfit(Still_FoamBottomInter)
+# Plotly_SidewallInter <- Weatherfit(Still_SidewallInter)
+# Plotly_FoamSidewallInter <- Weatherfit(Still_FoamSidewallInter)
+# Plotly_DirDiff <- subplot(Plotly_BottomHeat, Plotly_SidewallHeat, Plotly_BottomInter, Plotly_FoamBottomInter, Plotly_SidewallInter, Plotly_FoamSidewallInter, nrows = 3, shareX = TRUE)
+# Plotly_DirDiff
+
+
+Weatherfit <- function(StillData){
+  DirDiffStill <- StillData[, c("Global_Energy_Tot", "Global_Efficiency")]
+  DirDiffStill$DirDiffRatio <- StillData$Direct_Energy_Tot/StillData$Diffuse_Energy_Tot
+  DirDiffStill$Weather <- cut(DirDiffStill$DirDiffRatio, breaks = c(min(na.omit(DirDiffStill$DirDiffRatio)), 0.1, 1, max(na.omit(DirDiffStill$DirDiffRatio))), labels = c("Overcast (Dir./Diff. < 0.1)", "Cloudy (0.1 < Dir./Diff. < 1)", "Sunny (Dir./Diff. > 1)"))
+  return(DirDiffStill)
+}
+Still_DirDiff <- rbind(cbind(Weatherfit(Still_BottomHeat), StillType = "Bottom_Heating"), 
+                       cbind(Weatherfit(Still_SidewallHeat), StillType = "Sidewall_Bottom_Heating"),                        cbind(Weatherfit(Still_BottomInter), StillType = "Bottom_Interfacial"), 
+                       cbind(Weatherfit(Still_FoamBottomInter), StillType = "Foam_Bottom_Interfacial"), 
+                       cbind(Weatherfit(Still_SidewallInter), StillType = "Sidewall_Interfacial"), 
+                       cbind(Weatherfit(Still_FoamSidewallInter), StillType = "Foam_Sidewall_Interfacial")) 
+
+ggplot_DirDiff <- ggplot(data = na.omit(Still_DirDiff), aes(Global_Energy_Tot, Global_Efficiency*100, color = Weather)) + 
+  geom_point() + geom_smooth(method = "lm", se = FALSE) + scale_y_continuous(breaks=seq(0,60,5)) + 
+  labs(x = "Solar Energy/kWh", y = "Efficiency/%") + facet_wrap(. ~StillType, ncol = 2 )
+ggplotly(ggplot_DirDiff, height = 700)
+
+
+
 
 library(shiny)
 library(ggplot2)
